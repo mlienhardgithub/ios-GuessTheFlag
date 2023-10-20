814b08075d732040bde68e75113fe65fb6a2ca61
Martin <mlienhard@indeed.com>
2023-10-20 16:00:08 -0700

    first commit

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,9 +8,70 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+        .shuffled()
+    @State private var correctAnswer: Int = Int.random(in: 0...2)
+    @State private var showingScore: Bool = false
+    @State private var scoreTitle: String = ""
+    @State private var score: Int = 0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundColor(.white)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                }
+                ForEach(0 ..< 3) { number in
+                    Button(action: {
+                        //flag was tapped
+                        self.flagTapped(number)
+                    }) {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                            .clipShape(Capsule())
+                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                            .shadow(color: .black, radius: 12)
+                    }
+                    .accessibilityIdentifier(countries[number])
+                }
+                VStack {
+                    Text("Current score is \(score)")
+                        .foregroundColor(.white)
+                }
+                Spacer()
+            }
+        }
+        .alert(isPresented: $showingScore) {
+            Alert(
+                title: Text(scoreTitle),
+                message: Text("Your score is \(score)"),
+                dismissButton: .default(Text("Continue")) {
+                    self.askQuestion()
+                }
+            )
+        }
+    }
+    
+    func flagTapped (_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+            score += 1
+        } else {
+            scoreTitle = "Wrong! That’s the flag of \(countries[number])"
+        }
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
--- GuessTheFlagUITests/GuessTheFlagUITests.swift
+++ GuessTheFlagUITests/GuessTheFlagUITests.swift
@@ -8,6 +8,9 @@
 import XCTest
 
 class GuessTheFlagUITests: XCTestCase {
+    private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    private var incorrectScoreTitle:String = "Wrong! That’s the flag of"
+    private var correctScoreTitle:String = "Correct"
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -21,14 +24,29 @@ class GuessTheFlagUITests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
+    override class func setUp() {
+            // UI tests must launch the application that they test.
+            let app = XCUIApplication()
+            app.launchArguments = ["enable-testing"]
+            app.launch()
+        }
 
     func testExample() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
-        app.launch()
 
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        app.buttons["Russia"].tap()
+        
+        let alert = app.alerts["\(incorrectScoreTitle) Italy"].scrollViews.otherElements
+        alert.staticTexts["\(incorrectScoreTitle) Italy"].swipeRight()
+        alert.buttons["Continue"].tap()
+        app.buttons["button"].tap()
+        app.alerts["\(incorrectScoreTitle) Russia"].scrollViews.otherElements.buttons["Continue"].tap()
+                
     }
 
     func testLaunchPerformance() throws {
